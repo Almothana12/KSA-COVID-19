3eda60fecd68617e8718286f8acc24d07e9c008b
Almothana <43178491+Almothana12@users.noreply.github.com>
2020-04-26 17:09:25 +0300

    increased the rows
    increased the returnd rows from 70 to 100 and added a try catch

--- js/regions.js
+++ js/regions.js
@@ -15,40 +15,51 @@ getRegion("Al+Bahah");
 getRegion("Al+Jouf");
 
 async function getRegion(region) {
-    const api_url = "https://datasource.kapsarc.org/api/records/1.0/search/?dataset=saudi-arabia-coronavirus-disease-covid-19-situation&rows=70&sort=date&refine.daily_cumulative=Cumulative&refine.region=" + region;
-    const response = await fetch(api_url);
-    const data = await response.json();
+    try {
+        rows = 100;
+        const api_url = "https://datasource.kapsarc.org/api/records/1.0/search/?dataset=saudi-arabia-coronavirus-disease-covid-19-situation&rows=" + rows + "&sort=date&refine.daily_cumulative=Cumulative&refine.region=" + region;
+        const response = await fetch(api_url);
+        const data = await response.json();
 
-    const date = data.records[0].fields.date; // this holds the most recent date in the api
-    var cases = 0; activeCases = 0; mortalities = 0; recoveries = 0;
-    for (let i = 0; data.records[i].fields.date == date; i++) {
-        switch (data.records[i].fields.indicator) {
-            case "Cases":
-                cases += data.records[i].fields.cases;
+        const date = data.records[0].fields.date; // this holds the most recent date in the api
+        var cases = 0; activeCases = 0; mortalities = a0; recoveries = 0;
+        for (let i = 0; i < rows; i++) {
+            if (data.records[i].fields.date != date) {
                 break;
-            case "Active cases":
-                activeCases += data.records[i].fields.cases;
-                break;
-            case "Mortalities":
-                mortalities += data.records[i].fields.cases;
-                break;
-            case "Recoveries":
-                recoveries += data.records[i].fields.cases;
-                break;
-            default:
-                console.log("Unkown indicator:" + data.records[i].fields.indicator);
-                break;
+            }
+            switch (data.records[i].fields.indicator) {
+                case "Cases":
+                    cases += data.records[i].fields.cases;
+                    break;
+                case "Active cases":
+                    activeCases += data.records[i].fields.cases;
+                    break;
+                case "Mortalities":
+                    mortalities += data.records[i].fields.cases;
+                    break;
+                case "Recoveries":
+                    recoveries += data.records[i].fields.cases;
+                    break;
+                default:
+                    console.error("Unkown indicator:" + data.records[i].fields.indicator);
+                    break;
+            }
         }
-    }
-    region = region.replace("+", ""); //remove the plus sign from the region string
-    document.getElementById(region + "TotalCases").textContent = cases;
-    document.getElementById(region + "ActiveCases").textContent = activeCases;
-    document.getElementById(region + "Mortalities").textContent = mortalities;
-    document.getElementById(region + "Recovered").textContent = recoveries;
+        region = region.replace("+", ""); //remove the plus sign from the region string
+        document.getElementById(region + "TotalCases").textContent = cases;
+        document.getElementById(region + "ActiveCases").textContent = activeCases;
+        document.getElementById(region + "Mortalities").textContent = mortalities;
+        document.getElementById(region + "Recovered").textContent = recoveries;
 
-    finished++;
-    if (finished == 13) { // if this is the last region to load
-        // hide the loading spinner and show the body
+        finished++;
+        if (finished == 13) { // if this is the last region to load
+            // hide the loading spinner and show the body
+            document.getElementById("spinner").style.display = "none";
+            document.getElementById("regionBody").style.display = "inline";
+        }
+
+    } catch (error) {
+        console.error(error);
         document.getElementById("spinner").style.display = "none";
         document.getElementById("regionBody").style.display = "inline";
     }
