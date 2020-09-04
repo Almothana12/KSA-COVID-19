55aed10bac0ea3fe6e67b59979e80f5ee9630c59
Almothana <43178491+Almothana12@users.noreply.github.com>
2020-09-04 19:43:35 +0300

    updated region names and json attributes
    for some reason KAPSARC changed some region names again...
    cases -> case_value
    Active cases -> Active
    Riyadh -> Ar Riyad
    Mecca -> Makkah Al Mukarramah
    Medina -> Al Madinah Al Munawwarah
    Qassim -> Al Qaseem
    Asir -> Aseer

--- js/regions.js
+++ js/regions.js
@@ -1,26 +1,27 @@
 var finished = 0;
 
-getRegion("Riyadh");
-getRegion("Mecca");
-getRegion("Medina");
-getRegion("Qassim");
+getRegion("Ar+Riyad");
+getRegion("Makkah+Al+Mukarramah");
+getRegion("Al+Madinah+Al+Munawwarah");
+getRegion("Al+Qaseem");
 getRegion("Eastern+Region");
-getRegion("Asir");
+getRegion("Aseer");
 getRegion("Tabuk");
 getRegion("Hail");
 getRegion("Northern+Borders");
 getRegion("Jazan");
 getRegion("Najran");
 getRegion("Al+Bahah");
-getRegion("Al+Jouf");
+getRegion("Al+Jawf");
 
 async function getRegion(region) {
     try {
         rows = 150;
-        const api_url = "https://datasource.kapsarc.org/api/records/1.0/search/?dataset=saudi-arabia-coronavirus-disease-covid-19-situation&rows=" + rows + "&sort=date&refine.daily_cumulative=Cumulative&refine.region=" + region;
+        const api_url = "https://datasource.kapsarc.org/api/records/1.0/search/?dataset=saudi-arabia-coronavirus-disease-covid-19-situation&rows=" + rows + "&sort=date&refine.daily_cumulative=Cumulative&refine.region_en=" + region;
         const response = await fetch(api_url);
         const data = await response.json();
 
+        console.log()
         const date = data.records[0].fields.date; // this holds the most recent date in the api
         var cases = 0; activeCases = 0; mortalities = 0; recoveries = 0;
         for (let i = 0; i < rows; i++) {
@@ -29,23 +30,25 @@ async function getRegion(region) {
             }
             switch (data.records[i].fields.indicator) {
                 case "Cases":
-                    cases += data.records[i].fields.cases;
+                    cases += data.records[i].fields.case_value;
                     break;
-                case "Active cases":
-                    activeCases += data.records[i].fields.cases;
+                case "Active":
+                    activeCases += data.records[i].fields.case_value;
                     break;
                 case "Mortalities":
-                    mortalities += data.records[i].fields.cases;
+                    mortalities += data.records[i].fields.case_value;
                     break;
                 case "Recoveries":
-                    recoveries += data.records[i].fields.cases;
+                    recoveries += data.records[i].fields.case_value;
                     break;
                 default:
                     console.error("Unkown indicator:" + data.records[i].fields.indicator);
                     break;
             }
         }
-        region = region.replace("+", ""); //remove the plus sign from the region string
+        
+        region = region.split("+").join(""); //remove all the plus signs from the region string
+
         document.getElementById(region + "TotalCases").textContent = cases;
         document.getElementById(region + "ActiveCases").textContent = activeCases;
         document.getElementById(region + "Mortalities").textContent = mortalities;
